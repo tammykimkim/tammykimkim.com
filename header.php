e4a937c1f3eff7b4b4f46e7d7d6fd4e97a0693cd
Tammy Kim <tam@sapphirepixel.com>
2015-03-14 12:05:12 -0400

    made nav horizontal

--- header.php
+++ header.php
@@ -44,31 +44,39 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'one-page-theme' ); ?></a>
-
 	<header id="masthead" class="site-header" role="banner">
+		<div id="page" class="hfeed site">
+			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'one-page-theme' ); ?>
+			</a>
+
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="site-title">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?>
+				</a>
+			</h1>
 
 <!-- Cursor logo goes here -->
 		<div class="logo-box">
 			<h1>
-				<div class="logo">&gt;<span class="cursor">/</span>&lt;
+				<div class="logo">
+				&gt;<span class="cursor">/</span>&lt;
 				</div>
 			</h1>
+
 			<div class="logo-name">
 				tammykimkim
 			</div>
 		</div> <!-- end .logo-box -->
 
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<h2 class="site-description">
+				<?php bloginfo( 'description' ); ?>
+			</h2>
 		</div>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Primary Menu', 'one-page-theme' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
+</header><!-- #masthead -->
